f5d9a33e6986a9cd4a2476f778934158c6006e25
walaaelgenidy <45464802+walaaelgenidy@users.noreply.github.com>
2020-12-07 16:14:22 +0200

    Update stop_server.sh

--- stop_server.sh
+++ stop_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 isExistApp=`pgrep nginx`
 if [[ -n  $isExistApp ]]; then
-   service nginx stop
+   sudo systemctl stop nginx
 fi
 isExistApp=`pgrep mysqld`
 if [[ -n  $isExistApp ]]; then
